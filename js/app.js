fead1f23bdeb2fa9b9fea17c7f6fd3ed1fead956
Dadore76 <jkmilodark@gmail.com>
2022-12-08 22:13:46 -0500

    Change createItem() and markChecked

--- js/app.js
+++ js/app.js
@@ -14,40 +14,52 @@ let loadItems = () => {
 }
 
 let createItem = (item) => {
-    let html = ` <div class="item" id=${item.id}>
-                    <i class="bi bi-check-square" id="icon${item.id}" onclick="markChecked(this)"></i>
-                    <span>${item.description}</span>
-                    <i class="bi bi-trash" id="del${item.id}" onclick="delItem(this)"></i>
+    let status = item.status ? " item_checked" : "";
+    let checkIcon = item.status ? "-fill" : "";
+        
+    let html = `<div class="item${status}" id=${item.id}>
+                    <label class="check_item" onclick="markChecked(this)" id="lbl${item.id}">
+                        <i class="bi bi-check-square${checkIcon}" id="chk${item.id}"></i>
+                        <span>${item.description}</span>
+                    </label>
+                    <i class="bi bi-trash icon_del" onclick="delItem(this)" id="del${item.id}"></i>
                 </div>`;
     return html;
 }
 
-let addItem = () => {
-    let form = document.forms["form"];
-    let newItem = form["item"];
+// let addItem = () => {
+//     let form = document.forms["form"];
+//     let newItem = form["item"];
 
-    items.push(new Item(newItem.value))
-    newItem.value = "";
+//     items.push(new Item(newItem.value))
+//     newItem.value = "";
 
-    loadItems();
-}
+//     loadItems();
+// }
 
-let markChecked = (icon) => {
-    let id = icon.id.replace("icon", "");
+let markChecked = (item) => {
+    let id = item.id.replace("lbl", "");
+    let itemIndex = items.findIndex(item => item.id == id);
+    let icon = document.getElementById(`chk${id}`);
+
     let div = document.getElementById(id);
 
     div.classList.toggle("item_checked");
-
     icon.classList.toggle("bi-check-square-fill");
     icon.classList.toggle("bi-check-square");
-}
-
-let delItem = (icon) => {
-    let id = icon.id.replace("del", "");
-    let deleteIndex = items.findIndex(item => item.id == id);
-
-    items.splice(deleteIndex, 1);
     
-    loadItems();
+    if (div.classList.contains("item_checked"))
+        items[itemIndex].status = true;
+    else
+        items[itemIndex].status = false;
 }
+
+// let delItem = (icon) => {
+//     let id = icon.id.replace("del", "");
+//     let deleteIndex = items.findIndex(item => item.id == id);
+
+//     items.splice(deleteIndex, 1);
+    
+//     loadItems();
+// }
 
